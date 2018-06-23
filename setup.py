d7a1eaca1217ea61cb10fb95d7de3bf3552429bd
hex <hex@gamesci.com.cn>
2018-06-23 18:40:11 +0800

    ci

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(  
       name='pymsgq',
-      version='0.1.6',
+      version='0.1.7',
       description="A simple System V msgq interface for linux IPC (msgq)",
       keywords='system V msgq linux IPC msgsnd msgrcv msgget',  
       author='jj4jj',
